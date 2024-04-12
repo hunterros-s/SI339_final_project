173438ea43fe75d569c9c4da4dd35985b5f3eadf
hlross <hlross@umich.edu>
2024-04-12 09:49:44 -0400

    add persistent color between page refreshes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 var color_picker = document.getElementById("color-picker");
 var color_picker_wrapper = document.getElementById("color-picker-wrapper");
 
+function set_color(color) {
+    document.body.style.setProperty("--main-color", color);
+    localStorage.setItem("mainColor", color);
+    color_picker.value = color
+    color_picker_wrapper.style.backgroundColor = color; 
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const storedColor = localStorage.getItem("mainColor");
+
+    if (storedColor) {
+        set_color(storedColor);
+    } else {
+        const defaultColor = "#ff5e00";
+        set_color(defaultColor);
+    }
+});
 
 color_picker.addEventListener("input", function() {
-    document.body.style.setProperty('--main-color',this.value)
-    color_picker_wrapper.style.backgroundColor = color_picker.value;    
+    set_color(this.value);
 })
 
 color_picker_wrapper.style.backgroundColor = color_picker.value;
