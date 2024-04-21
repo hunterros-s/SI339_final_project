3348121f89231f9d97b7844b296f278a2f8920e6
hlross <hlross@umich.edu>
2024-04-21 18:40:01 -0400

    add dark theme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// selection color, main color theme
 function set_color(color) {
     document.documentElement.style.setProperty("--main-color", color);
     localStorage.setItem("mainColor", color);
@@ -23,7 +24,6 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 const storedColor = localStorage.getItem("mainColor");
-    
 if (storedColor) {
     set_color(storedColor);
 } else {
@@ -31,4 +31,47 @@ if (storedColor) {
     set_color(defaultColor);
 }
 
-// var dark_theme_button = document.getElementById("dark-theme")
+// dark theme
+function set_dark_theme() {
+    document.documentElement.style.setProperty("--text-color", "#fff");
+    document.documentElement.style.setProperty("--background-color", "#1f1f21");
+    document.documentElement.style.setProperty("--outline-color", "#2d2d2d");
+}
+function set_light_theme() {
+    document.documentElement.style.setProperty("--text-color", "#232333");
+    document.documentElement.style.setProperty("--background-color", "#fff");
+    document.documentElement.style.setProperty("--outline-color", "#BCBEC0");
+}
+
+const theme_enabled = localStorage.getItem("darkTheme");
+var enabled;
+if (theme_enabled == "true") {
+    enabled = true;
+} else {
+    enabled = false;
+}
+var checked;
+if (enabled) {
+    set_dark_theme()
+    checked = true;
+} else {
+    set_light_theme()
+    checked = false;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    var dark_theme_checker = document.getElementById("dark-theme")
+
+    dark_theme_checker.addEventListener("input", function() {
+        var checked = this.checked
+        localStorage.setItem("darkTheme", checked);
+
+        if (checked) {
+            set_dark_theme()
+        } else {
+            set_light_theme()
+        }
+    })
+
+    dark_theme_checker.checked = checked
+});
