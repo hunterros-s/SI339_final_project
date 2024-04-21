4c2bc0bd39b859c2c502aa79463fe210d030c9d0
hlross <hlross@umich.edu>
2024-04-21 18:13:23 -0400

    fix so colors are set before dom loads

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-var color_picker = document.getElementById("color-picker");
-var color_picker_wrapper = document.getElementById("color-picker-wrapper");
-
 function set_color(color) {
-    document.body.style.setProperty("--main-color", color);
+    document.documentElement.style.setProperty("--main-color", color);
     localStorage.setItem("mainColor", color);
-    color_picker.value = color
-    color_picker_wrapper.style.backgroundColor = color; 
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+window.addEventListener('DOMContentLoaded', () => {
     const storedColor = localStorage.getItem("mainColor");
+    var color_picker = document.getElementById("color-picker");
+    var color_picker_wrapper = document.getElementById("color-picker-wrapper");
 
-    if (storedColor) {
-        set_color(storedColor);
-    } else {
-        const defaultColor = "#ff5e00";
-        set_color(defaultColor);
+    function update_color(color) {
+        set_color(color)
+
+        color_picker.value = color
+        color_picker_wrapper.style.backgroundColor = color; 
     }
+
+    update_color(storedColor)
+    
+    color_picker.addEventListener("input", function() {
+        update_color(this.value);
+    })
 });
 
-color_picker.addEventListener("input", function() {
-    set_color(this.value);
-})
+const storedColor = localStorage.getItem("mainColor");
+    
+if (storedColor) {
+    set_color(storedColor);
+} else {
+    const defaultColor = "#ff5e00";
+    set_color(defaultColor);
+}
+
+// var dark_theme_button = document.getElementById("dark-theme")
